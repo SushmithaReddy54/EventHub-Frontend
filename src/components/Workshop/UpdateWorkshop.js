ea8448a10f58e84ffc450703b0f02f4e219391fd
Sushmitha Reddy Kariveda <sushmithareddykariveda@Sushmithas-Air.lan>
2024-12-11 17:49:39 -0500

    latest update

--- src/components/Workshop/UpdateWorkshop.js
+++ src/components/Workshop/UpdateWorkshop.js
@@ -228,7 +228,7 @@ const UpdateWorkshop = () => {
                     fullWidth
                     variant="contained"
                     component="label"
-                    disabled
+                   // disabled
                     sx={{ display: "block", textAlign: "center", py: 2 }}
                   >
                     Upload Image
